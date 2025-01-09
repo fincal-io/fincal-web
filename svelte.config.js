81eca85ce4462aea984d48d33f1938732ac5428a
Your Name <you@example.com>
2025-01-10 00:03:45 +0530

    remove github actions

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '',
+			fallback: 'index.html',
 			precompress: true,
 			strict: true
 		})
--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		extensions: ['.js', '.ts', '.svelte'],
-		alias: {
-			'@hugeicons/core': '@hugeicons/core/dist'
-		}
-	},
-	optimizeDeps: {
-		include: ['@hugeicons/core']
-	},
-	build: {
-		commonjsOptions: {
-			include: [/@hugeicons\/svelte/]
-		}
-	}
+	plugins: [sveltekit()]
 });
